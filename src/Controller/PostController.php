f7c96122042a38d9e8e1e38198f86773fe3223f9
brankojanjic <branko.janjic@fsd.rs>
2018-11-18 00:54:00 +0100

    First commit

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -10,7 +10,6 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\VarDumper\VarDumper;
 
 /**
  * @Route("/api/posts")
